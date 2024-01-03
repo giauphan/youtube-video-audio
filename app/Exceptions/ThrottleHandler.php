f0a4d9fac74cea422c260fdb617c583f5c585458
giauphan <giauphan@users.noreply.github.com>
2024-01-03 11:48:22 +0000

    chore: fix code style

--- app/Exceptions/ThrottleHandler.php
+++ app/Exceptions/ThrottleHandler.php
@@ -2,7 +2,6 @@
 
 namespace App\Exceptions;
 
-
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Illuminate\Http\Response;
 use Symfony\Component\HttpKernel\Exception\TooManyRequestsHttpException;
@@ -12,8 +11,8 @@ class ThrottleHandler extends ExceptionHandler
     public function render($request, \Throwable $exception)
     {
         if ($exception instanceof TooManyRequestsHttpException) {
-            return redirect()->to('/')->with('error', "Too Many Requests")
-                ->setStatusCode(Response::HTTP_TOO_MANY_REQUESTS);;
+            return redirect()->to('/')->with('error', 'Too Many Requests')
+                ->setStatusCode(Response::HTTP_TOO_MANY_REQUESTS);
         }
 
         return parent::render($request, $exception);
