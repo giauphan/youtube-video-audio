83b22ed5e76d98bf778dfd50ec8ab5debe65d33e
giauphan <giauphan012@gmail.com>
2024-01-12 16:34:22 +0700

    upadte role

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 
+use Filament\Models\Contracts\FilamentUser;
 use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -11,7 +12,7 @@ use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasApiTokens, HasFactory, HasRoles,Notifiable;
 
@@ -46,8 +47,8 @@ class User extends Authenticatable
         'password' => 'hashed',
     ];
 
-    // public function canAccessPanel(Panel $panel): bool
-    // {
-    //     return $this->hasRole(['Admin']);
-    // }
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return $this->hasRole(['Admin']);
+    }
 }
