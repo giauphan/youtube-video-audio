c7dd172db221d8e8f1216d63aa9a3bbe489b60de
giauphan <giauphan012@gmail.com>
2024-01-12 13:53:34 +0700

    fix ERR DB index is out of range

--- config/database.php
+++ config/database.php
@@ -129,7 +129,7 @@ return [
         ],
 
         'default' => [
-            'url' => env('REDIS_URL'),
+            // 'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'username' => env('REDIS_USERNAME'),
             'password' => env('REDIS_PASSWORD'),
@@ -143,7 +143,7 @@ return [
             'username' => env('REDIS_USERNAME'),
             'password' => env('REDIS_PASSWORD'),
             'port' => env('REDIS_PORT', '6379'),
-            'database' => env('REDIS_CACHE_DB', '1'),
+            'database' => env('REDIS_CACHE_DB', '0'),
         ],
 
     ],
