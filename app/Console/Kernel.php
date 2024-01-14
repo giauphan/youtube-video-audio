bc461693ef8fddbcaebfa6fcbdd03b89e73af133
giauphan <giauphan012@gmail.com>
2024-01-14 14:55:21 +0700

    update run queue and bot

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         $schedule->command('bot:check-video')->everyMinute();
-        $schedule->command('queu:work')->everyMinute();
+        $schedule->command('queu:work')->everyMinute()->withoutOverlapping();
     }
 
     /**
