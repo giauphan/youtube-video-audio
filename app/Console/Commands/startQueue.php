3bbc7f5f49e452400f1c6bda65ba8b4cd712fda5
giauphan <giauphan012@gmail.com>
2024-01-19 12:26:07 +0700

    update status andstart queue:work

---
+++ app/Console/Commands/startQueue.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+use Symfony\Component\Process\Process;
+
+class startQueue extends Command
+{  /**
+    * The name and signature of the console command.
+    *
+    * @var string
+    */
+   protected $signature = 'start:queue';
+
+   /**
+    * The console command description.
+    *
+    * @var string
+    */
+   protected $description = 'Command description';
+
+   /**
+    * Execute the console command.
+    */
+   public function handle()
+   {
+       $check = Artisan::call('status:queue');
+
+       if ($check) {
+           $this->info('Scanning files is already running. Use "scan:scan-link" to check its status.');
+           return true;
+       }
+
+       // Set the timezone to Asia/Ho_Chi_Minh
+       date_default_timezone_set('Asia/Ho_Chi_Minh');
+
+       $projectPath = base_path();
+
+       $this->info('Starting queue work...' . now());
+
+       $cmd = getenv('Path_PHP') . " " . $projectPath . '/artisan queue:work';
+       $process = Process::fromShellCommandline($cmd);
+       $process->setTimeout(null);
+       $process->run();
+
+       if ($process->isSuccessful()) {
+           $this->info('Scanning files completed successfully. and sleep waint 60s');
+           return ;
+       } else {
+           $this->error('An error occurred while scanning files: ' . $process->getErrorOutput() . now());
+           return 1;
+       }
+   }
+}
---
+++ resources/js/Pages/Post/Index.vue
@@ -0,0 +1,40 @@
+<template>
+<div className="mx-auto my-10 max-w-5xl px-4 sm:px-6 md:my-20 md:px-8">
+                <div className="mb-10 flex flex-col  bg-gray-100 px-4 py-3">
+                    <h2 className="mb-4 text-xl font-bold"></h2>
+                    <ul className="flex flex-wrap gap-x-3 gap-y-2 text-gray-600">
+                            <li :key="category.id" className="font-semibold transition-all hover:text-primary-500" v-for="category in categorys">
+                                <Link :href="route('posts.show',[`?tag=${category.slug}`])">{{category.name}}</Link>
+                            </li>
+                    </ul>
+                </div>
+                <div className="flex flex-col justify-center gap-y-10">
+                        <div key={post.id} className="flex flex-col gap-4" v-for="post in posts">
+                            <h2 className="relative text-3xl font-bold transition-all before:absolute before:-start-10 before:top-0 before:text-gray-900/10 before:content-['#'] hover:text-primary-500 hover:before:text-primary-500 sm:text-4xl lg:text-5xl">
+                                <a :href="route('posts.show', post.slug)">{post.title}</a>
+                            </h2>
+                            <p>{{post.description}}</p>
+                            <div className="flex gap-6">
+                                <div>
+                                    bởi
+                                    <strong className="ms-1">{{post.user.name}}</strong>
+                                </div>
+                                <div className="flex gap-2">
+                                        <a :href="route('posts.show', [`?tag=${tag.slug}`])" :key="tag.id" v-for="tag in tags">
+                                            #{{tags.name}}
+                                        </a>
+                                </div>
+                                <div>{{post.published_at}}</div>
+                            </div>
+                        </div>
+                </div>
+
+                <Paginate :meta="posts.meta" />
+            </div>
+</template>
+
+<script setup>
+
+
+const props = defineProps(['posts','tags','categorys'])
+</script>
