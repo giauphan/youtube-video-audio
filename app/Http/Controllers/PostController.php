a807c9bd3d0cd0580e3bf1d4ecb82347e8062c98
giauphan <giauphan@users.noreply.github.com>
2024-01-19 08:30:29 +0000

    chore: fix code style

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,22 +14,22 @@ class PostController extends Controller
     public function index(Request $request)
     {
         $validated = Validator::validate($request->all(), [
-            'category_blog_name' => ['nullable', 'string']
+            'category_blog_name' => ['nullable', 'string'],
         ]);
 
         $categoryBlog = CategoryBlog::query()->get();
 
         $posts = Post::query()
-        ->when(Arr::get($validated, 'category_blog_name'), function(Builder $query,string $category_sug){
-            $query->whereHas('CategoryBlog', fn (Builder $query) =>$query->where('slug',$category_sug));
-        })
-        ->orderBy('view', 'desc')
-        ->paginate(8)
-        ->withQueryString();
+            ->when(Arr::get($validated, 'category_blog_name'), function (Builder $query, string $category_sug) {
+                $query->whereHas('CategoryBlog', fn (Builder $query) => $query->where('slug', $category_sug));
+            })
+            ->orderBy('view', 'desc')
+            ->paginate(8)
+            ->withQueryString();
 
-        return view('Post.Index',[
-            'categoryBlog'=>$categoryBlog,
-            'posts'=>$posts
+        return view('Post.Index', [
+            'categoryBlog' => $categoryBlog,
+            'posts' => $posts,
         ]);
     }
 }
