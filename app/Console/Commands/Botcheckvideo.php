6f2a8c6071cc0cad36fc936728d5b40f74222470
giauphan <giauphan012@gmail.com>
2024-01-19 14:36:59 +0700

    update check link after add report

--- app/Console/Commands/Botcheckvideo.php
+++ app/Console/Commands/Botcheckvideo.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use App\Jobs\ReportJob;
 use App\Models\YoutubeVideo;
 use Carbon\Carbon;
+use GuzzleHttp\Client;
 use Illuminate\Console\Command;
 
 class Botcheckvideo extends Command
@@ -28,15 +29,32 @@ class Botcheckvideo extends Command
      */
     public function handle()
     {
-        $this->info('Starting bot refresh link video...'.now());
+        $this->info('Starting bot refresh link video...' . now());
         $getVideo = YoutubeVideo::query()->get();
         $N = 0;
         while ($N < 2) {
-
-            foreach ($getVideo as $key => $videos) {
-                ReportJob::dispatch($videos['video_id'], $videos['type'])->delay(Carbon::now()->addSeconds(5));
+            foreach ($getVideo as  $videos) {
+                if (!$this->checkLinkStatus($videos->url_video)) {
+                    ReportJob::dispatch($videos['video_id'], $videos['type'])->delay(Carbon::now()->addSeconds(5));
+                }
             }
             $N++;
+        }
+    }
+
+
+    public function checkLinkStatus($url)
+    {
+        try {
+            $client = new Client(['timeout' => 2]);
+
+            $client->get($url);
+        } catch (\Exception $e) {
+            if ($e->getCode() == 0) {
+                return false;
+            }
+
+            return true;
         }
     }
 }
