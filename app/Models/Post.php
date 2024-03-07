febf8bb71170dfed15ef6f2ad2ffb0eb88742235
giauphan <giauphan012@gmail.com>
2024-03-07 16:47:28 +0700

    set feat search for category post and post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,11 +4,26 @@ namespace App\Models;
 
 use Giauphan\CrawlBlog\Models\Post as ModelsPost;
 use Illuminate\Database\Eloquent\Builder;
+use Laravel\Scout\Searchable;
 
 class Post extends ModelsPost
 {
+    use Searchable;
+
     public function scopePublished(Builder $query): Builder
     {
         return $query->where('published_at', '<=', now());
     }
+
+    public function toSearchableArray()
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'summary' => $this->summary,
+            'tags' => $this->tags,
+            'published_at' => $this->published_at,
+        ];
+    }
+    
 }
