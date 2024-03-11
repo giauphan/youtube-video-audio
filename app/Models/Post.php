351f4fccf3bd9849d3e10971fcd13820b5bfb4c9
giauphan <giauphan012@gmail.com>
2024-03-11 14:33:55 +0700

    fix comfic

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,31 +4,14 @@ namespace App\Models;
 
 use Giauphan\CrawlBlog\Models\Post as ModelsPost;
 use Illuminate\Database\Eloquent\Builder;
-<<<<<<< Updated upstream
-=======
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
-use Laravel\Scout\Searchable;
->>>>>>> Stashed changes
 
 class Post extends ModelsPost
 {
     public function scopePublished(Builder $query): Builder
     {
         return $query->where('published_at', '<=', now());
-    }
-<<<<<<< Updated upstream
-=======
-
-    public function toSearchableArray()
-    {
-        return [
-            'id' => $this->id,
-            'title' => $this->title,
-            'summary' => $this->summary,
-            'tags' => $this->tags,
-            'published_at' => $this->published_at,
-        ];
     }
 
     public function comments(): MorphMany
@@ -39,5 +22,4 @@ class Post extends ModelsPost
     public function user():BelongsTo{
         return $this->belongsTo(User::class);
     }
->>>>>>> Stashed changes
 }
